6d9dd1ed68f9995763c89dc6aeaf8ab5e24c4e5f
ardan-developler <55392685+ardan-developler@users.noreply.github.com>
2019-09-25 21:19:32 +0900

    Update index.js

--- index.js
+++ index.js
@@ -45,39 +45,6 @@ bot.on("message", async message => {
   }
 })
 
-bot.on("message", async message => {
-  if(message.author.bot) return;
-  if(message.channel.type === "dm") return;
 
-  let prefix = '+';
-  let messageArray = message.content.split(" ");
-  let cmd = messageArray[0];
-  let args = messageArray.slice(1);
-
-  if(cmd === `${prefix}userinfo`){
-    let user = message.mentions.users.first() || message.author;
-
-    let userinfo = {};
-    userinfo.avatar = user.displayAvatarURL()
-    userinfo.name = user.username;
-    userinfo.discrim - `#${user.discriminator}`;
-    userinfo.id = user.id;
-    userinfo.status = user.presence.status;
-    userinfo.registered = moment.utc(message.guild.members.get(user.id).user.createdAt).format("dddd, MMMM do, YYYY");
-    userinfo.joined = moment.utc(message.guild.members.get(user.id).joinedAt).format("dddd, MMMM Do, YYYY");
-
-    let userembed = new Discord.RichEmbed()
-    .setAuthor(user.tag, userinfo.avatar)
-    .setThumbnail(userinfo.avatar)
-    .addField(`username`, userinfo.name, true)
-    .addField(`Discriminator`, userinfo.discrim, true)
-    .addField(`ID`, userinfo.id, true)
-    .addField(`Status`, userinfo.status, true)
-    .addField(`Account Creation Date`, userinfo.registered)
-    .addField(`Server Joined`, userinfo.joined)
-
-    return message.channel.send(userembed);
-  }
-})
 
 bot.login("NjI1MTYxODkyNTc0Mzk2NDI3.XYtKYQ.7HL_6L2urR0ZSMahVNobfFlHSoQ");
